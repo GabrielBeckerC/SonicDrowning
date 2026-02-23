8315551462bfb1a9dc973cb639af1ce2fc4ce214
GabrielBeckerC <gabeccosta@gmail.com>
2026-02-23 11:18:02 -0300

    Adding code to git

---
+++ README.md
@@ -0,0 +1,18 @@
+🌀 Sonic Drowning Music for WoW 🌊
+"Gotta go fast... to the surface!" 💨
+Ever felt that the default World of Warcraft breath meter is a bit... too chill? Does it lack that adrenaline-pumping urgency that tells you every millisecond counts?
+
+This Addon brings the iconic (and terrifying) drowning music from the Sonic the Hedgehog franchise directly into your WoW experience. When oxygen runs low, the panic begins!
+
+✨ Features
+Sonic Drowning Music silently monitors your breath meter. As soon as you hit the critical 13-second mark, the classic Sonic panic track kicks in.
+
+Nostalgic Panic: Experience the same pressure you felt playing on the Sega Genesis.
+
+Version Agnostic: Built with a smart logic that works across Retail, Classic Era, TBC, and Cataclysm.
+
+Seamless Integration: Designed to work alongside major UI addons like RXP (RestedXP) and ElvUI. It automatically handles the different decimal time scales found in various game versions.
+## Instalation 
+1. Download and add to folder:
+   `_retail_/Interface/AddOns/`
+3. Reload the game.
---
+++ core.lua
@@ -0,0 +1,37 @@
+local threshold = 14
+local musicPath = "Interface\\AddOns\\SonicDrowning\\Sounds\\sonic-1-music-drownings.mp3"
+local musicPlaying = false
+
+local frame = CreateFrame("Frame")
+frame:SetScript("OnUpdate", function(self, elapsed)
+    if MirrorTimer1:IsShown() and MirrorTimer1.timer == "BREATH" then
+        
+        local secondsLeft = MirrorTimer1.value 
+
+        if secondsLeft < 1 then
+            secondsLeft = secondsLeft * (MirrorTimer1.maxvalue or 60)
+        end
+
+        -- Lógica de disparo
+        if secondsLeft <= threshold and secondsLeft > 0 then
+            if not musicPlaying then
+                PlayMusic(musicPath)
+                musicPlaying = true
+            end
+        elseif secondsLeft > threshold and musicPlaying then
+            StopMusic()
+            musicPlaying = false
+        end
+    else
+        if musicPlaying then
+            StopMusic()
+            musicPlaying = false
+        end
+    end
+end)
+
+SLASH_SONIC1 = "/sonic"
+SlashCmdList["SONIC"] = function()
+    PlayMusic(musicPath)
+    C_Timer.After(5, function() StopMusic() end)
+end
